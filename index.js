101522f78739f5657f9ddecec3b74d6ccf5a1b7f
Matt Lee <mattleesounds@gmail.com>
2022-04-13 13:06:59 -0500

    Continued ethers.js implementation

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-import { StrictMode } from "react"
-import ReactDOM from "react-dom"
-
-import ImplementSigning from "./ ImplementSigning"
-
-const connectWalletElement = document.getElementById("connect-wallet-container")
-ReactDOM.render(
-    <StrictMode>
-        <ImplementSigning />
-    </StrictMode>,
-    connectWalletElement
-)
+//import { ethers } from "ethers";
 
 //Grab audio and video elements
 let savasanaAudioEl = document.getElementById("savasana-file")
@@ -168,3 +157,25 @@ function toggleBtnToPlay(buttonIndex){
     buttonEl[buttonIndex].style.background = "url('play-btn2.png')";
     buttonEl[buttonIndex].style.backgroundSize = "100%";
 }
+
+/*
+//------------------------------------------------------------------------
+//Sign Message
+function signMessage(){
+    // A Web3Provider wraps a standard Web3 provider, which is
+    // what MetaMask injects as window.ethereum into each page
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+    // MetaMask requires requesting permission to connect users accounts
+    await provider.send("eth_requestAccounts", []);
+
+    // The MetaMask plugin also allows signing transactions to
+    // send ether and pay to change state within the blockchain.
+    // For this, you need the account signer...
+    const signer = provider.getSigner()
+}
+
+function a() {
+    console.log(aaaaaaaaaaaaaa)
+}
+*/
