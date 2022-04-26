9ffcf5df70ee51116bc7e82454a74085ece6347a
Matt Lee <mattleesounds@gmail.com>
2022-04-26 18:06:21 -0500

    Created array to store wallet addresses

--- signMessage.js
+++ signMessage.js
@@ -1,4 +1,4 @@
-const { ethers } = require("ethers");
+//const { ethers } = require("ethers");
 
 //const INFURA_ID = '2d6784d79edc4227ab8ad282653ac8a8'
 //const provider = new ethers.providers.JsonRpcProvider('https://mainnet.infura.io/v3/${INFURA_ID}')
@@ -9,17 +9,56 @@ const provider = new ethers.providers.Web3Provider(window.ethereum)
 
 //------------------------------------------------------------------------
 //Sign Message
+
+let addressLog = []
+
 async function signMessage(){
+
+    const message = "This message verifies proof of listenership for the Serene Wrap EP"
     
+    if (!window.ethereum){
+        throw new Error("No crypto wallet found. Please install it.")
+    }
     // MetaMask requires requesting permission to connect users accounts
-    await provider.send("eth_requestAccounts", []);
+    await provider.send("eth_requestAccounts", [])
 
     // The MetaMask plugin also allows signing transactions to
     // send ether and pay to change state within the blockchain.
     // For this, you need the account signer...
     const signer = await provider.getSigner()
+
+    const signature = await signer.signMessage(message)
+    const address = await signer.getAddress()
+
+    addressLog += address + ", "
+    console.log(addressLog)
+
 }
 
+
+/*
+const signMessage = async ({ setError, message }) => {
+    try {
+      console.log({ message });
+      if (!window.ethereum)
+        throw new Error("No crypto wallet found. Please install it.");
+  
+      await window.ethereum.send("eth_requestAccounts");
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const signature = await signer.signMessage(message);
+      const address = await signer.getAddress();
+  
+      return {
+        message,
+        signature,
+        address
+      };
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+*/
 //function a() {
 //    console.log(aaaaaaaaaaaaaa)
 //}
