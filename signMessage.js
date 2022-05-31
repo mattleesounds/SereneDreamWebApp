99e80dcead2865712b674ad0bbc7ea9fdf2f44c2
Matt Lee <mattleesounds@gmail.com>
2022-05-31 13:40:36 -0500

    Implementation of MySQL database

--- signMessage.js
+++ signMessage.js
@@ -1,7 +1,7 @@
 //const { ethers } = require("ethers");
 
-//const INFURA_ID = '2d6784d79edc4227ab8ad282653ac8a8'
-//const provider = new ethers.providers.JsonRpcProvider('https://mainnet.infura.io/v3/${INFURA_ID}')
+//Link mysql2 library to store user info
+var mysql = require('mysql2');
 
 // A Web3Provider wraps a standard Web3 provider, which is
 // what MetaMask injects as window.ethereum into each page
@@ -10,30 +10,46 @@ const provider = new ethers.providers.Web3Provider(window.ethereum)
 //------------------------------------------------------------------------
 //Sign Message
 
-let addressLog = []
-
-async function signMessage(){
+async function sign(){
 
     const message = "This message verifies proof of listenership for the Serene Wrap EP"
     
     if (!window.ethereum){
         throw new Error("No crypto wallet found. Please install it.")
     }
+    
     // MetaMask requires requesting permission to connect users accounts
     await provider.send("eth_requestAccounts", [])
-
+    
     // The MetaMask plugin also allows signing transactions to
     // send ether and pay to change state within the blockchain.
     // For this, you need the account signer...
     const signer = await provider.getSigner()
-
     const signature = await signer.signMessage(message)
     const address = await signer.getAddress()
+    
+    //Check if message has been verified from this address
+    //If so, connect to listeners database, then add address and sigature to listener_data table
+    if (address == ethers.util.verifyMessage(message, signature)){
+      var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "leonardo11",
+        database: "listeners"
+      });
+      
+      con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        var sql = "INSERT INTO listener_data (publicKey, signature) VALUES (?, ?)";
+        con.query(sql, [address, signature], function (err, result) {
+          if (err) throw err;
+          console.log("1 record inserted");
+        });
+      });
+    }
 
-    ethers.util.verifyMessage(message, signature)
-
-    addressLog += address + ", "
-    console.log(addressLog)
+   
 
 }
 
